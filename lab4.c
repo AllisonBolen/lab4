095b9e762ba917716f9e0d5d3d5fb97805db5309
Allison Bolen <bolena@mail.gvsu.edu>
2018-02-07 10:52:06 -0500

    hsdfuhsdilf

--- lab4.c
+++ lab4.c
@@ -13,15 +13,15 @@ struct sockaddr_ll addr, listenaddr;
 listenaddr.sll_family=AF_PACKET; // family exists for conversion stuill tell waht it originally was
 listenaddr.sll_protocol=htons(ETH_P_ALL);
 listenaddr.sll_ifindex=if_nametoindex("h1-eth0");//interface  try not to hard code this is for mininet
-bind(sockfd,(struct sockaddr*))&listenaddr,sizeof(listenaddr);
+bind(sockfd,(struct sockaddr*)&listenaddr,sizeof(listenaddr));
 
 while(1){
   int len = sizeof(addr);
   char buf[5000];
-  int n = recvfrom(sockfd,buf,0,(struct sockaddr*)&addr, &len); //set up recive?
+  int n = recvfrom(sockfd,buf,5000,0,(struct sockaddr*)&addr, &len); //set up recive?
 
   if(addr.sll_pkttype!=PACKET_OUTGOING){
-    
+
     printf("GOT A PACKET\n");
 
     struct ether_header eh;
